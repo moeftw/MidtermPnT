f7a484bdff8b82b82fafd1c5ca7a4fe15aba4dc2
moetaseen <moe.taseen@gmail.com>
2023-04-10 22:44:29 -0400

    Answers

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -1,3 +1,4 @@
+
 package string.problems;
 
 public class Permutation {
@@ -9,5 +10,29 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+
+        String str = "ABC";
+        permute(str, 0, str.length() - 1);
+    }
+
+    public static void permute(String str, int left, int right) {
+        if (left == right) {
+            System.out.println(str);
+        } else {
+            for (int i = left; i <= right; i++) {
+                str = swap(str, left, i);
+                permute(str, left + 1, right);
+                str = swap(str, left, i);
+            }
+        }
+    }
+
+    public static String swap(String str, int i, int j) {
+        char temp;
+        char[] charArray = str.toCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
     }
 }
