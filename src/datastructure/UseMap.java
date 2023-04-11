567f11b7207f3c7d54f31deaf189d5aeac0bcf4a
moetaseen <moe.taseen@gmail.com>
2023-04-10 23:14:59 -0400

    final

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,16 +1,11 @@
 package datastructure;
-
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
 import databases.ConnectToSqlDB;
 
+import java.util.*;
 public class UseMap {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+
 		/*
 		 * Demonstrate how to use Map that includes storing and retrieving elements.
 		 * Add List<String> into a Map. Like, Map<String, List<string>> list = new HashMap<String, List<String>>();
@@ -18,12 +13,12 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
-		Map<String, List<String>> list = new HashMap<String, List<String>>();
+
 
 		HashMap<Object, Object> map = new HashMap<>();
-		map.put(2120, "TASEEN");
-		map.put(2038, "ZACK");
-		map.put(8467, "DAVID");
+		map.put(2120, "Mohammad");
+		map.put(2038, "Osama");
+		map.put(8467, "Baruch");
 
 
 		Iterator<Object> mapIt = map.keySet().iterator();
@@ -34,20 +29,12 @@ public class UseMap {
 			System.out.println("VALUE: " + map.get(key));
 		}
 
+
 		String tableName = "`test_hash_map`";
 		ConnectToSqlDB sql = new ConnectToSqlDB();
 
-		try {
-			sql.insertDataFromMap(tableName, map);
-			System.out.println(sql.readDataBase(tableName));
-			sql.updateDataInTable(tableName, "8467", "David Smith");
-			System.out.println(sql.readDataBase(tableName));
-			sql.deleteRow(tableName, "8467");
-			System.out.println(sql.readDataBase(tableName));
-		} catch (SQLException e) {
-			System.out.println("SQL Exception: " + e.getMessage());
-		}
 
 	}
-
 }
+
+
